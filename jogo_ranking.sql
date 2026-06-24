bc3c1dfab877aa8aa60ff5e60bb9515c3b587057
guifogometeoro <bragacam13@gmail.com>
2026-06-24 09:26:07 -0300

    Update jogo_ranking.sql

--- jogo_ranking.sql
+++ jogo_ranking.sql
@@ -18,7 +18,8 @@
 --
 -- Table structure for table `ranking`
 --
-
+create database jogo;
+use jogo;
 DROP TABLE IF EXISTS `ranking`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
