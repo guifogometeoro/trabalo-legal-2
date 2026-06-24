cf59acf458a34dd1571b695e63801f3a07606217
guifogometeoro <bragacam13@gmail.com>
2026-06-24 09:27:18 -0300

    Update zalina_produtos.sql

--- zalina_produtos.sql
+++ zalina_produtos.sql
@@ -18,7 +18,8 @@
 --
 -- Table structure for table `produtos`
 --
-
+create database zalina;
+use zalina;
 DROP TABLE IF EXISTS `produtos`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
