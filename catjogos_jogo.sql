0993fb0be8b596eed51f59d1fa840967ee5192c1
guifogometeoro <bragacam13@gmail.com>
2026-06-24 09:25:22 -0300

    Update catjogos_jogo.sql

--- catjogos_jogo.sql
+++ catjogos_jogo.sql
@@ -18,7 +18,8 @@
 --
 -- Table structure for table `jogo`
 --
-
+create database catjogos;
+use catjogos;
 DROP TABLE IF EXISTS `jogo`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
